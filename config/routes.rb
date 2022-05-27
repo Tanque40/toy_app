d1d217ad78a540e27cbe087669a94c6e42ea3b48
Bruno Vitte <bruno_vitte@hotmail.com>
2022-05-27 17:22:48 +0000

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,3 +7,4 @@ Rails.application.routes.draw do
   
   root 'application#hello'
 end
+  
